fe9030a2bd285f59ef33cbfb2d482e699f7075dc
pdb5627 <pdbrown@prd.nppdnet.com>
2016-06-20 15:05:19 -0500

    Clean up SplitByHighlighting.py
    Cleaned up code to pass default flake8 checks.

--- SplitByHighlighting.py
+++ SplitByHighlighting.py
@@ -17,17 +17,42 @@ is run from.
 
 from __future__ import print_function, unicode_literals
 
-from WordHelpers import *
+from WordHelpers import remove_highlighted, iter_all_runs
 
-import re, sys, os.path, os, codecs, msvcrt, shutil
+import re
+import sys
+import os.path
+import os
+import codecs
+import msvcrt
+import shutil
 
 # Set up a logger so any errors can go to file to facilitate debugging
 import logging
 from logging.config import dictConfig
 
-# By default, log to the same directory the program is run from    
+#
+# The docx package documentation can be found at
+# https://python-docx.readthedocs.org/en/latest/
+#
+# Installation:
+# If pip is available:
+# pip install python-docx
+#
+# If easy_install is available:
+# easy_install python-docx
+#
+# Otherwise download the tar.gz and run setup.py install.
+# Currently working with 0.8.5. Version 0.7.2 NOT working.
+# As of 5/27/2016, I needed the development branch in order to get the
+# highlighting code.
+#
+from docx import Document
+
+# By default, log to the same directory the program is run from
 if os.path.exists(os.path.dirname(sys.argv[0])):
-    logfile = os.path.join(os.path.dirname(sys.argv[0]), 'SplitByHighlighting.log')
+    logfile = os.path.join(os.path.dirname(sys.argv[0]),
+                           'SplitByHighlighting.log')
 else:
     logfile = 'SplitByHighlighting.log'
 
@@ -35,91 +60,73 @@ logging_config = {
     'version': 1,
     'formatters': {
         'file': {'format':
-              '%(asctime)s ' + os.environ['USERNAME'] + ' %(levelname)-8s %(message)s'},
+                 '%(asctime)s ' + os.environ['USERNAME'] +
+                 ' %(levelname)-8s %(message)s'},
         'console': {'format':
-              '%(levelname)-8s %(message)s'}
+                    '%(levelname)-8s %(message)s'}
         },
-        
+
     'handlers': {
         'file': {'class': 'logging.FileHandler',
-            'filename': logfile,
-            'formatter': 'file',
-            'level': 'INFO'},
+                 'filename': logfile,
+                 'formatter': 'file',
+                 'level': 'INFO'},
         'console': {'class': 'logging.StreamHandler',
-            'formatter': 'console',
-            'level': 'INFO'}
+                    'formatter': 'console',
+                    'level': 'INFO'}
         },
     'loggers': {
-        'root' : {'handlers': ['file', 'console'],
-            'level': 'DEBUG'}
+        'root': {'handlers': ['file', 'console'],
+                 'level': 'DEBUG'}
         }
 }
 
 dictConfig(logging_config)
 
 logger = logging.getLogger('root')
-    
+
 try:
     logger.info('Running %s.' % sys.argv[0])
     logger.info('Logging to file %s.' % os.path.abspath(logfile))
 
-    '''
-    The docx package documentation can be found at
-    https://python-docx.readthedocs.org/en/latest/
-
-    Installation:
-    If pip is available:
-    pip install python-docx
-
-    If easy_install is available:
-    easy_install python-docx
-
-    Otherwise download the tar.gz and run setup.py install.
-    Currently working with 0.8.5. Version 0.7.2 NOT working.
-    As of 5/27/2016, I needed the development branch in order to get the
-    highlighting code.
-    '''
-    from docx import Document
-    from docx.enum.text import WD_COLOR_INDEX
-
     debug = False
-    sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout, errors='replace')
+    sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout,
+                                                       errors='replace')
 
     if len(sys.argv) < 2:
-        logger.error("Not enough input parameters.  Please include one filename when calling this program.")
+        logger.error("Not enough input parameters.  Please include one "
+                     "filename when calling this program.")
         logger.error(__doc__)
         raise SystemExit
     elif len(sys.argv) > 2:
-        logger.error("Too many input parameters.  Please include one filename when calling this program.")
+        logger.error("Too many input parameters.  Please include one filename "
+                     "when calling this program.")
         logger.error(__doc__)
         raise SystemExit
 
     documentParam = sys.argv[1]
 
-
-    doc_base = re.match('(.*)\.doc[xm]$',documentParam, flags=re.I).group(1)
+    doc_base = re.match('(.*)\.doc[xm]$', documentParam, flags=re.I).group(1)
 
     logger.info('Input file: ' + documentParam)
     logger.info('Base filename:  ' + doc_base)
-    #logger.info('Output files: ' + sel_save_file + ', ' + aspen_save_file)
-
 
     document = Document(documentParam)
 
     run_dict = dict()
 
     for p, r in iter_all_runs(document):
-        if r.font.highlight_color != None:
+        if r.font.highlight_color is not None:
             logger.debug(r.text + ' ===> ' + str(r.font.highlight_color))
             try:
                 run_dict[r.font.highlight_color].append(r)
             except KeyError:
                 run_dict[r.font.highlight_color] = [r]
-    
+
     logger.debug('Colors used and number of runs')
     for k, v in run_dict.items():
         logger.debug(str(k) + ': ' + str(len(v)))
-        
+
     # For each color, make a temp copy of the document, clear text in runs
     # of different highlighting colors and write the output to a new file.
     all_colors = set(run_dict.keys())
@@ -136,9 +143,8 @@ except (SystemExit, KeyboardInterrupt):
     raise
 except Exception, e:
     logger.error('Program error', exc_info=True)
-finally:    
-    # Code copied from http://stackoverflow.com/questions/11876618/python-press-any-key-to-exit    
-    # This uses a Windows-specific library (msvrt).
+finally:
     logger.info('DONE.')
     print('Press any key to exit...')
-    junk = msvcrt.getch() # Assign to a variable just to suppress output. Blocks until key press.
+    # Assign to a variable just to suppress output. Blocks until key press.
+    junk = msvcrt.getch()
