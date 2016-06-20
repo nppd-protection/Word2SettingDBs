c98e7442e2240fb6d7a31357324b6d99fc0855d4
pdb5627 <pdbrown@prd.nppdnet.com>
2016-06-20 14:56:16 -0500

    Clean up make_line_relay_templates.py
    Cleaned up code to pass default flake8 checks.

--- make_line_relay_templates.py
+++ make_line_relay_templates.py
@@ -2,8 +2,10 @@
 
 '''
 USAGE:
-make_line_relay_templates.py <Settings Dual SEL-421 Line Relay Master Standard.docx>
-<Settings Dual SEL-421 Line Relay Master Standard.docx>:  MS Word docx document,
+make_line_relay_templates.py ...
+    <Settings Dual SEL-421 Line Relay Master Standard.docx>
+
+<Settings Dual SEL-421 Line Relay Master Standard.docx>:  MS Word docx document
     master template for line relays.
 
 ABOUT:
@@ -17,26 +19,36 @@ is run from.
 
 from __future__ import print_function, unicode_literals
 
-from WordHelpers import *
+from WordHelpers import find_replace, remove_highlighted, \
+    get_bookmark_par_element
 
-import re, sys, os.path, os, codecs, msvcrt, shutil
+import re
+import sys
+import os.path
+import os
+import codecs
+import shutil
 
-'''
-The docx package documentation can be found at
-https://python-docx.readthedocs.org/en/latest/
+# Set up a logger so any errors can go to file to facilitate debugging
+import logging
+from logging.config import dictConfig
 
-Installation:
-If pip is available:
-pip install python-docx
-
-If easy_install is available:
-easy_install python-docx
-
-Otherwise download the tar.gz and run setup.py install.
-Currently working with 0.8.5. Version 0.7.2 NOT working.
-As of 5/27/2016, I needed the development branch in order to get the
-highlighting code.
-'''
+#
+# The docx package documentation can be found at
+# https://python-docx.readthedocs.org/en/latest/
+#
+# Installation:
+# If pip is available:
+# pip install python-docx
+#
+# If easy_install is available:
+# easy_install python-docx
+#
+# Otherwise download the tar.gz and run setup.py install.
+# Currently working with 0.8.5. Version 0.7.2 NOT working.
+# As of 5/27/2016, I needed the development branch in order to get the
+# highlighting code.
+#
 from docx import Document
 
 # See https://python-docx.readthedocs.io/en/latest/api/enum/WdColorIndex.html
@@ -48,7 +60,8 @@ from docx.enum.text import WD_COLOR_INDEX
 # Assigned highlighting colors in this document:
 #   wdTurquoise (3):    DCB (PLC) Settings
 #   wdBrightGreen (4):  POTT (Mirrored Bits) Settings
-#   wdGray25(16):       115/230 Settings (Non-piloted secondary, SEL-421-4, etc.)
+#   wdGray25(16):       115/230 Settings (Non-piloted secondary, SEL-421-4,
+#                       etc. )
 #   wdRed (6):          DCUB Primary / POTT Secondary
 #   wdTeal (10):        SEL-411L Primary / POTT Secondary
 #   wdDarkYellow (14):  SEL-421 Primary
@@ -81,22 +94,22 @@ all_colors = set([
 
 # Colors to remove for each standard.
 colors_to_remove = {
-    #DCBPriNonPilotSecOneBreaker
+    # DCBPriNonPilotSecOneBreaker
     'PP115-230E1A3A': [
         TwoBkr,
         DCUBPriPOTTSec,
         POTTPri,
         SEL411LPriPOTTSec],
 
-    #POTTPriNonPilotSecOneBreaker
+    # POTTPriNonPilotSecOneBreaker
     'PP115-230E1A3B': [
         TwoBkr,
         DCUBPriPOTTSec,
         DCBPri,
         SEL411LPriPOTTSec],
 
-    #411LPriPOTTSecOneBreaker
-    #ChangePri421to411L
+    # 411LPriPOTTSecOneBreaker
+    # ChangePri421to411L
     'PP115-230E1A3C': [
         TwoBkr,
         NonPilotSec,
@@ -105,22 +118,22 @@ colors_to_remove = {
         POTTPri,
         SEL421Pri],
 
-    #DCBPriNonPilotSecTwoBreaker
+    # DCBPriNonPilotSecTwoBreaker
     'PP115-230E1B3A': [
         OneBkr,
         DCUBPriPOTTSec,
         POTTPri,
         SEL411LPriPOTTSec],
 
-    #POTTPriNonPilotSecTwoBreaker
+    # POTTPriNonPilotSecTwoBreaker
     'PP115-230E1B3B': [
         OneBkr,
         DCUBPriPOTTSec,
         DCBPri,
         SEL411LPriPOTTSec],
-    
-    #411LPriPOTTSecTwoBreaker
-    #ChangePri421to411L
+
+    # 411LPriPOTTSecTwoBreaker
+    # ChangePri421to411L
     'PP115-230E1B3C': [
         OneBkr,
         NonPilotSec,
@@ -129,7 +142,7 @@ colors_to_remove = {
         POTTPri,
         SEL421Pri],
 
-    #DCUBPriPOTTSecTwoBreaker
+    # DCUBPriPOTTSecTwoBreaker
     'PP115-230E1B3A': [
         OneBkr,
         DCBPri,
@@ -142,7 +155,7 @@ colors_to_remove = {
 all_colors = set([OneBkr, TwoBkr,
                   DCBPri, POTTPri, SEL411LPriPOTTSec, DCUBPriPOTTSec,
                   SEL421Pri, NonPilotSec])
-                  
+
 colors_to_keep = {
     'PP115-230E1A3A': set([SEL421Pri, DCBPri,  NonPilotSec, OneBkr]),
     'PP115-230E1A3B': set([SEL421Pri, POTTPri, NonPilotSec, OneBkr]),
@@ -152,17 +165,25 @@ colors_to_keep = {
     'PP115-230E1B3C': set([SEL411LPriPOTTSec,               TwoBkr]),
     'PP345J0X5A':     set([SEL421Pri, DCBPri,  NonPilotSec, TwoBkr]),
     'TEST':           set()}
-    
+
 std_filenames = {
-    'PP115-230E1A3A': 'Settings PP115-230E1A3A RFL9785-DCB 21P-02 11S-02 SEL-421-4',
-    'PP115-230E1A3B': 'Settings PP115-230E1A3B MB-POTT 21P-02 11S-02 SEL-421-4',
+    'PP115-230E1A3A': 'Settings PP115-230E1A3A RFL9785-DCB 21P-02 11S-02 '
+                      'SEL-421-4',
+    'PP115-230E1A3B': 'Settings PP115-230E1A3B MB-POTT 21P-02 11S-02 '
+                      'SEL-421-4',
     'PP115-230E1A3C': 'Settings PP115-230E1A3C 87P-02 11S-02 SEL-411L-421-4',
-    'PP115-230E1B3A': 'Settings PP115-230E1B3A RFL9785-DCB 21P-L1098 21S-L1098 SEL-421-4',
-    'PP115-230E1B3B': 'Settings PP115-230E1B3B MB-POTT 21P-L1369 21S-L1369 SEL-421-4',
-    'PP115-230E1B3C': 'Settings PP115-230E1B3C 87P-L91A 21S-L91A SEL-411L-421-4',
-    'PP345J0X5A':     'Settings PP345J0X5A 345kv line relays 21P-21S SEL-421-5',
-    'TEST':           'Settings Dual SEL-421 Line Relay master Standard (macro test)'}
-    
+    'PP115-230E1B3A': 'Settings PP115-230E1B3A RFL9785-DCB 21P-L1098 21S-L1098'
+                      ' SEL-421-4',
+    'PP115-230E1B3B': 'Settings PP115-230E1B3B MB-POTT 21P-L1369 21S-L1369 '
+                      'SEL-421-4',
+    'PP115-230E1B3C': 'Settings PP115-230E1B3C 87P-L91A 21S-L91A '
+                      'SEL-411L-421-4',
+    'PP345J0X5A':     'Settings PP345J0X5A 345kv line relays 21P-21S '
+                      'SEL-421-5',
+    'TEST':           'Settings Dual SEL-421 Line Relay master Standard '
+                      '(macro test)'}
+
+
 def change_pri_421_to_411L(document):
     replace_list = [('OUT2', 'OUT3'),
                     ('OUT1', 'OUT2'),
@@ -172,10 +193,12 @@ def change_pri_421_to_411L(document):
                     ('IBXM', 'IBXFM'),
                     ('ICXM', 'ICXVM'),
                     ('51S1T', '51T01')]
-    replace_start = get_bookmark_par_element(document, 'SecondarySettingsStart')
+    replace_start = get_bookmark_par_element(document,
+                                             'SecondarySettingsStart')
     for find_text, replace_text in replace_list:
         find_replace(document, find_text, replace_text, end=replace_start)
     find_replace(document, '87P-0X', '87P-LZZ')
+
 
 def make_line_relay_templates(document, std):
     colors_to_remove = all_colors - colors_to_keep[std]
@@ -184,13 +207,10 @@ def make_line_relay_templates(document, std):
     if SEL411LPriPOTTSec in colors_to_keep[std]:
         change_pri_421_to_411L(document)
 
-# Set up a logger so any errors can go to file to facilitate debugging
-import logging
-from logging.config import dictConfig
-
-# By default, log to the same directory the program is run from    
+# By default, log to the same directory the program is run from
 if os.path.exists(os.path.dirname(sys.argv[0])):
-    logfile = os.path.join(os.path.dirname(sys.argv[0]), 'make_line_relay_templates.log')
+    logfile = os.path.join(os.path.dirname(sys.argv[0]),
+                           'make_line_relay_templates.log')
 else:
     logfile = 'SplitByHighlighting.log'
 
@@ -198,23 +218,24 @@ logging_config = {
     'version': 1,
     'formatters': {
         'file': {'format':
-              '%(asctime)s ' + os.environ['USERNAME'] + ' %(levelname)-8s %(message)s'},
+                 '%(asctime)s ' + os.environ['USERNAME'] +
+                 ' %(levelname)-8s %(message)s'},
         'console': {'format':
-              '%(levelname)-8s %(message)s'}
+                    '%(levelname)-8s %(message)s'}
         },
-        
+
     'handlers': {
         'file': {'class': 'logging.FileHandler',
-            'filename': logfile,
-            'formatter': 'file',
-            'level': 'INFO'},
+                 'filename': logfile,
+                 'formatter': 'file',
+                 'level': 'INFO'},
         'console': {'class': 'logging.StreamHandler',
-            'formatter': 'console',
-            'level': 'DEBUG'}
+                    'formatter': 'console',
+                    'level': 'DEBUG'}
         },
     'loggers': {
-        'root' : {'handlers': ['file', 'console'],
-            'level': 'DEBUG'}
+        'root': {'handlers': ['file', 'console'],
+                 'level': 'DEBUG'}
         }
 }
 
@@ -226,24 +247,24 @@ try:
     logger.info('Running %s.' % sys.argv[0])
     logger.info('Logging to file %s.' % os.path.abspath(logfile))
 
-
-
     debug = False
-    sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout, errors='replace')
+    sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout,
+                                                       errors='replace')
 
     if len(sys.argv) < 2:
-        logger.error("Not enough input parameters.  Please include one filename when calling this program.")
+        logger.error("Not enough input parameters.  Please include one "
+                     "filename when calling this program.")
         logger.error(__doc__)
         raise SystemExit
     elif len(sys.argv) > 2:
-        logger.error("Too many input parameters.  Please include one filename when calling this program.")
+        logger.error("Too many input parameters.  Please include one filename "
+                     "when calling this program.")
         logger.error(__doc__)
         raise SystemExit
 
     documentParam = sys.argv[1]
 
-
-    doc_base = re.match('(.*)\.doc[xm]$',documentParam, flags=re.I).group(1)
+    doc_base = re.match('(.*)\.doc[xm]$', documentParam, flags=re.I).group(1)
 
     logger.info('Input file: ' + documentParam)
     logger.info('Base filename:  ' + doc_base)
@@ -251,19 +272,20 @@ try:
     # For testing, hard-code standard to use
     std = 'PP115-230E1A3A'
     for std in std_filenames.keys():
-        
+
         try:
-            file_rev = re.search(' Rev ?[0-9]+$', doc_base, flags=re.I).group(0)
+            file_rev = re.search(' Rev ?[0-9]+$', doc_base,
+                                 flags=re.I).group(0)
         except AttributeError:
             file_rev = ''
-            
-        save_file = os.path.join(os.path.dirname(documentParam), std_filenames[std] + file_rev + '.docx')
+
+        save_file = os.path.join(os.path.dirname(documentParam),
+                                 std_filenames[std] + file_rev + '.docx')
         shutil.copyfile(documentParam, save_file)
         document = Document(save_file)
-        
+
         make_line_relay_templates(document, std)
 
-        
         logger.info('Saving output file: %s' % save_file)
         document.save(save_file)
 
@@ -271,9 +293,5 @@ except (SystemExit, KeyboardInterrupt):
     raise
 except Exception, e:
     logger.error('Program error', exc_info=True)
-finally:    
-    # Code copied from http://stackoverflow.com/questions/11876618/python-press-any-key-to-exit    
-    # This uses a Windows-specific library (msvrt).
+finally:
     logger.info('DONE.')
-    print('Press any key to exit...')
-    #junk = msvcrt.getch() # Assign to a variable just to suppress output. Blocks until key press.
