ff66ee6d70324a67d700217573518191965a19d3
pdb5627 <pdbrown@prd.nppdnet.com>
2016-06-20 14:15:49 -0500

    Clean up FindReplace.py
    Cleaned up code to pass default flake8 checks.

--- FindReplace.py
+++ FindReplace.py
@@ -15,15 +15,20 @@ is run from.
 
 from __future__ import print_function, unicode_literals
 
-from WordHelpers import *
+from WordHelpers import find_replace
 
-import re, sys, os.path, os, codecs, msvcrt, shutil
+import re
+import sys
+import os.path
+import os
+import codecs
+import shutil
 
 # Set up a logger so any errors can go to file to facilitate debugging
 import logging
 from logging.config import dictConfig
 
-# By default, log to the same directory the program is run from    
+# By default, log to the same directory the program is run from
 if os.path.exists(os.path.dirname(sys.argv[0])):
     logfile = os.path.join(os.path.dirname(sys.argv[0]), 'FindReplace.log')
 else:
@@ -33,23 +38,24 @@ logging_config = {
     'version': 1,
     'formatters': {
         'file': {'format':
-              '%(asctime)s ' + os.environ['USERNAME'] + ' %(levelname)-8s %(message)s'},
+                 '%(asctime)s ' + os.environ['USERNAME'] +
+                 ' %(levelname)-8s %(message)s'},
         'console': {'format':
-              '%(levelname)-8s %(message)s'}
+                    '%(levelname)-8s %(message)s'}
         },
-        
+
     'handlers': {
         'file': {'class': 'logging.FileHandler',
-            'filename': logfile,
-            'formatter': 'file',
-            'level': 'INFO'},
+                 'filename': logfile,
+                 'formatter': 'file',
+                 'level': 'INFO'},
         'console': {'class': 'logging.StreamHandler',
-            'formatter': 'console',
-            'level': 'DEBUG'}
+                    'formatter': 'console',
+                    'level': 'DEBUG'}
         },
     'loggers': {
-        'root' : {'handlers': ['file', 'console'],
-            'level': 'DEBUG'}
+        'root': {'handlers': ['file', 'console'],
+                 'level': 'DEBUG'}
         }
 }
 
@@ -78,41 +84,39 @@ try:
     highlighting code.
     '''
     from docx import Document
-    from docx.enum.text import WD_COLOR_INDEX
 
     debug = False
-    sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout, errors='replace')
+    sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout,
+                                                       errors='replace')
 
     if len(sys.argv) < 2:
-        logger.error("Not enough input parameters.  Please include one filename when calling this program.")
+        logger.error("Not enough input parameters.  Please include one "
+                     "filename when calling this program.")
         logger.error(__doc__)
         raise SystemExit
     elif len(sys.argv) > 2:
-        logger.error("Too many input parameters.  Please include one filename when calling this program.")
+        logger.error("Too many input parameters.  Please include one filename "
+                     "when calling this program.")
         logger.error(__doc__)
         raise SystemExit
 
     documentParam = sys.argv[1]
 
-
-    doc_base = re.match('(.*)\.doc[xm]$',documentParam, flags=re.I).group(1)
+    doc_base = re.match('(.*)\.doc[xm]$', documentParam, flags=re.I).group(1)
 
     logger.info('Input file: ' + documentParam)
     logger.info('Base filename:  ' + doc_base)
-    #logger.info('Output files: ' + sel_save_file + ', ' + aspen_save_file)
 
-    
     save_file = doc_base + ' (modified).docx'
     shutil.copyfile(documentParam, save_file)
     document = Document(save_file)
-    
+
     # Hardcoded for testing.
     find_text = 'test'
     replace_text = 'replaced'
-    
+
     find_replace(document, find_text, replace_text)
 
-    
     logger.info('Saving output file: %s' % save_file)
     document.save(save_file)
 
@@ -120,9 +124,5 @@ except (SystemExit, KeyboardInterrupt):
     raise
 except Exception, e:
     logger.error('Program error', exc_info=True)
-finally:    
-    # Code copied from http://stackoverflow.com/questions/11876618/python-press-any-key-to-exit    
-    # This uses a Windows-specific library (msvrt).
+finally:
     logger.info('DONE.')
-    print('Press any key to exit...')
-    #junk = msvcrt.getch() # Assign to a variable just to suppress output. Blocks until key press.
